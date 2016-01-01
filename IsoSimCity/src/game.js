47ab65ea2a48244429ca575710e0d1125ffc9cbd
Robert Smith <smurftheweb@gmail.com>
2016-01-01 22:18:03 +0000

    working phaser example in typescript

--- IsoSimCity/src/game.js
+++ IsoSimCity/src/game.js
@@ -1,26 +1,21 @@
-﻿/// <reference path="../libs/phaser/phaser.d.ts" />
+/// <reference path="../libs/phaser/phaser.d.ts" />
 /// <reference path="../libs/phaser/p2.d.ts" />
 /// <reference path="../libs/phaser/pixi.d.ts" />
 /// <reference path="../libs/phaser-isometric/phaser-plugin-isometric.d.ts" />
-
-class IsoGame {
-
-    game: Phaser.Game;
-
-    constructor() {
+var IsoGame = (function () {
+    function IsoGame() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
     }
-
-    preload() {
+    IsoGame.prototype.preload = function () {
         this.game.load.image('logo', 'assets/phaser2.png');
-    }
-
-    create() {
+    };
+    IsoGame.prototype.create = function () {
         var logo = this.game.add.image(this.game.world.centerX, this.game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
-    }
-}
-
-window.onload = () => {
+    };
+    return IsoGame;
+})();
+window.onload = function () {
     var game = new IsoGame();
 };
+//# sourceMappingURL=game.js.map
