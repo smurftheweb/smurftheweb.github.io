2eb700f6eade61aa024c0f2e0838215d1b11ba24
Robert Smith <smurftheweb@gmail.com>
2016-01-01 14:18:26 +0000

    add phaser + phaser isometric

---
+++ IsoSimCity/src/game.ts
@@ -0,0 +1,4 @@
+</// reference="/libs/phaser/phaser.d.ts" />
+</// reference="/libs/phaser/p2.d.ts" />
+</// reference="/libs/phaser/pixi.d.ts" />
+</// reference="/libs/phaser-isometric/phaser-plugin-isometric.d.ts" />
